a5449ff6d859f67a7e2681cca767158b08c568b2
Shivasai2888 <jalagarishivasai@gmail.com>
2025-10-31 23:26:30 +0530

    Updated main, exercise, string, and theory files in VS Code

---
+++ string.py
@@ -0,0 +1,16 @@
+# name = input("Enter your full  name: ")
+phone_number = input("Enter your phone number: ")
+
+# result = len(name)
+# result = name.find("s")
+# result = name.rfind("s")
+# result = name.rfind("q")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.alpha()
+# result = phone_number.count("-")
+result = phone_number.replace("3","2")
+
+print(result)
